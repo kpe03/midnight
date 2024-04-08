576eb97a5089af160565a772945d5addb4186bd2
KPeters <99045807+kpe03@user.noreply.github.com>
2024-04-08 16:12:38 -0500

    setting up the app

---
+++ frontend/src/assets/fonts.js
@@ -0,0 +1,9 @@
+import {Inter, Source_Serif_4} from 'next/font/google'
+
+export const sourceSerif = Source_Serif_4({
+    subsets: ['latin'],
+})
+
+export const inter = Inter({
+    subsets: ['latin']
+})
