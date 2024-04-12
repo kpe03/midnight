0f83f04875c5bfc979a4922eeb5c4d2b099dc2bb
KPeters <99045807+kpe03@user.noreply.github.com>
2024-04-12 14:37:45 -0500

    nav button

--- frontend/src/app/components/navbar.js
+++ frontend/src/app/components/navbar.js
@@ -1,10 +1,12 @@
 import style from '../assets/style.module.css'
 import React from 'react';
+import {Button } from 'react'
 
 export function NavBar() {
     return(
         <div className={style.navbody}>
             <p className={style.navTitle}>Midnight</p>
+            <button className={style.navButton} type='button'>Login</button>
         </div>
 
     );
--- frontend/src/app/views/Home.js
+++ frontend/src/app/views/Home.js
@@ -4,7 +4,12 @@ import style from '../assets/style.module.css'
 export function Home() {
     return(
         <div className={style.homePage}>
-            <p> welcome to homepage </p>
+            <div className='header'> 
+                <p className={style.homeh1}> Welcome </p>
+            </div>
+            <div className='forYou'>
+                <p className={style.homeh2}>For you</p>
+            </div>
         </div>
     );
 }
